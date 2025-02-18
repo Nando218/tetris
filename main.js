55710a09c16158c4abd61a8b1acfe20c8d35dbf2
Nando218 <fernando.delatorre.design@gmail.com>
2025-02-18 10:02:53 +0100

    initial commit: add Tetris game files including HTML, CSS, and JavaScript

---
+++ main.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('tetris');
+const ctx = canvas.getContext('2d');
+const pauseBtn = document.getElementById('pauseButton');
+const ScoreElement = document.getElementById('score');
+const highScore = document.getElementById('highScore');
